2314eb37700b05478f7cbd9fdc7ec827566b0500
przemek83 <4788832+przemek83@users.noreply.github.com>
2020-10-04 19:54:41 +0200

    Add test for complex network.

--- test/SocialNetworkTest.cpp
+++ test/SocialNetworkTest.cpp
@@ -18,3 +18,24 @@ TEST(SocialNetworkTest, oneNodeNetwork)
         {"Tim"}, 10U};
     EXPECT_EQ(socialNetwork.getMSTAndWeight(), expectedResult);
 }
+
+TEST(SocialNetworkTest, complexNetwork)
+{
+    SocialNetwork socialNetwork(5U);
+
+    socialNetwork.addPerson("Robert", 12U);
+    socialNetwork.addPerson("Julia", 23U);
+    socialNetwork.addPerson("Adam", 1U);
+    socialNetwork.addPerson("Carol", 10U);
+    socialNetwork.addPerson("Daniel", 4U);
+
+    socialNetwork.addConnection("Robert", "Julia");
+    socialNetwork.addConnection("Robert", "Carol");
+    socialNetwork.addConnection("Adam", "Robert");
+    socialNetwork.addConnection("Daniel", "Adam");
+    socialNetwork.addConnection("Daniel", "Julia");
+
+    std::pair<std::unordered_set<std::string>, unsigned int> expectedResult{
+        {"Adam", "Robert"}, 13U};
+    EXPECT_EQ(socialNetwork.getMSTAndWeight(), expectedResult);
+}
